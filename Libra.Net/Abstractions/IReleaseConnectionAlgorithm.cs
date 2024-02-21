8a6fc189f7ac1d21227f689a350b7da92b4d0d8b
Andrea Prestia <andrea@prestia.dev>
2024-02-21 13:14:14 +0100

    feat: add release connection method

---
+++ Libra.Net/Abstractions/IReleaseConnectionAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿using Libra.Net.Entities;
+
+namespace Libra.Net.Abstractions
+{
+    /// <summary>
+    /// This interface contains the part for release connection to implement for specific policy
+    /// </summary>
+    public interface IReleaseConnectionAlgorithm : ILoadBalancingAlgorithm
+    {
+        /// <summary>
+        /// Release a connection for the server
+        /// </summary>
+        /// <param name="server"></param>
+        void ReleaseConnection(Server? server);
+    }
+}
